46690afd09998fa275c0748a414fd9130f1e9e73
NiTika1509 <sicnax@gmail.com>
2023-04-28 15:44:19 +0300

    feat(controllers): created slider controller class

---
+++ controllers/Sliders.php
@@ -0,0 +1,23 @@
+<?php namespace Yamobile\Banners\Controllers;
+
+use Backend\Classes\Controller;
+use BackendMenu;
+
+class Sliders extends Controller
+{
+    public $implement = [
+        'Backend\Behaviors\ListController',
+        'Backend\Behaviors\FormController',
+        'Backend\Behaviors\ReorderController'
+    ];
+    
+    public $listConfig = 'config_list.yaml';
+    public $formConfig = 'config_form.yaml';
+    public $reorderConfig = 'config_reorder.yaml';
+
+    public function __construct()
+    {
+        parent::__construct();
+        BackendMenu::setContext('Yamobile.Banners', 'slider-menu');
+    }
+}
